a2f20296912a571424a9faf62a9e5ead020b943c
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-05-15 13:02:03 +0300

    working menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RadialMenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+export function RadialMenu() {
+  const [open, setOpen] = useState<boolean>(false);
+  const liElements = ["a", "b", "c", "d", "e", "f"];
+  return (
+    <>
+      <div className="flex">
+        <button onClick={() => setOpen(!open)}>X</button>
+        {open ? (
+          <div className="flex ml-3">
+            {liElements.map((el) => {
+              return <div className="ml-1">{el}</div>;
+            })}
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+}
